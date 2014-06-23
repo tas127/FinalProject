19ebdfbb35d5026e29978ba2a5a96f79dfd7b6c5
iD Student <Student@admins-iMac-4.local>
2014-06-23 10:13:58 -0400

    Frist Message

---
+++ FinalProjectThree/FinalProjectThree/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FinalProjectThree
+//
+//  Created by iD Student on 6/23/14.
+//  Copyright (c) 2014 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
